d85393526cf14be96a15c8d1dc1063dda350bb78
Eric Theuri <tericmail001@gmail.com>
2025-12-02 18:33:21 +0300

    Instagram scraper

---
+++ genai_service/utils/scrapers.py
@@ -0,0 +1,24 @@
+from apify_client import ApifyClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = ApifyClient(os.getenv("APIFY_TOKEN"))
+instagram_actor = client.actor("apify/instagram-scraper")
+
+def instagram_input(link):
+    run_input = { 
+    "directUrls": [link],
+    "resultsType": "posts",
+    "resultsLimit": 200,
+    "searchType": "hashtag",
+    "searchLimit": 1, 
+    }
+    return run_input
+
+def get_instagram_results():
+    raw_results = instagram_actor.call(run_input=instagram_input(link="https://www.instagram.com/reel/DRwZDbKjZaD/?utm_source=ig_web_copy_link&igsh=NTc4MTIwNjQ2YQ=="))
+
+    for item in client.dataset(raw_results["defaultDatasetId"]).iterate_items():
+        print(item)
